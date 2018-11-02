d9c440935adf205feda3a45db0f3ca5c6af4244e
mango <glstrive@163.com>
2018-11-02 17:53:31 +0800

    ant design

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { inject,observer } from 'mobx-react';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import { Input, Row, Col } from 'antd';
+import'./style.css'
 
 @inject( 'HomeStore' )
 @observer
@@ -10,15 +12,46 @@ class Home extends React.Component {
     actions = () => {
         this.props.HomeStore.changeVal();
     }
+    state = {
+        val: ''
+    }
+    changeCont = (e) => {
+        this.setState({
+            val: e.target.value
+        })
+    }
     render() {
         return (
-            <div>
-                <p onClick={this.actions}>点一下看效果</p>
-                <p>{this.props.HomeStore.title}</p>
-                <Link to="/me">跳转</Link>
+            <div className="container">
+                <Row>
+                    <Col span={8}>
+                        <Input onChange={this.changeCont} size="small" placeholder="请输入响应内容" />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p onClick={this.actions}>点一下看效果</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>{this.props.HomeStore.title}</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>{this.state.val}</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Link to="/me">跳转</Link>
+                    </Col>
+                </Row>
             </div>
         );
     }
 }
 
 export default Home;
+
